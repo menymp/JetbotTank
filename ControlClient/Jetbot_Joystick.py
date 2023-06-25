56a9fd9956ee87c21809ef67f8e105bb0f668295
menymp <lmeny04@hotmail.com>
2023-06-24 20:06:46 -0600

    refractor logic for joystic controller

--- ControlClient/Jetbot_Joystick.py
+++ ControlClient/Jetbot_Joystick.py
@@ -1,198 +1,135 @@
-"""
-# Sample Python/Pygame Programs
-# Simpson College Computer Science
-# http://programarcadegames.com/
-# http://simpson.edu/computer-science/
-"""
- 
-import pygame
-import socket
-import time
+'''
+menymp 24 jun 2023
 
-# Definimos algunos colores
-NEGRO = (0, 0, 0)
-BLANCO = (255, 255, 255)
+software to create a control client for the jetbot with a joystick
+'''
 
+import sys
+sys.path.append("../JetbotServer")
+
+from socketServerWrapper import socketClientW
+
+'''
 s = socket.socket()
 
 host = 'xxx.xxx.x.x'
 port = 8990 #1234
 s.connect((host, port))
+'''
  
-class TextPrint(object):
-    '''
-    Esta es una sencilla clase que nos ayudara a imprimir sobre la pantalla
-    No tiene nada que ver con los joysticks, tan solo imprime informacion
-    '''
-    def __init__(self):
-        """Constructor"""
-        self.reset()
-        self.x_pos = 10
-        self.y_pos = 10
-        self.font = pygame.font.Font(None, 20)
- 
-    def print(self, mi_pantalla, text_string):
-        textBitmap = self.font.render(text_string, True, NEGRO)
-        mi_pantalla.blit(textBitmap, [self.x, self.y])
-        self.y += self.line_height
-         
-    def reset(self):
-        self.x = 10
-        self.y = 10
-        self.line_height = 15
-         
-    def indent(self):
-        self.x += 10
-         
-    def unindent(self):
-        self.x -= 10
-     
- 
-pygame.init()
-  
-# Establecemos el largo y alto de la pantalla [largo,alto]
-dimensiones = [500, 700]
-pantalla = pygame.display.set_mode(dimensiones)
- 
-pygame.display.set_caption("Mi Juego")
- 
-#Iteramos hasta que el usuario pulsa el boton de salir.
-hecho = False
- 
-# Lo usamos para gestionar cuan rapido de refresca la pantalla.
-reloj = pygame.time.Clock()
- 
-# Inicializa los joysticks
-pygame.joystick.init()
-     
-# Se prepara para imprimir
-text_print = TextPrint()
- 
-# -------- Bucle Principal del Programa -----------
-while not hecho:
-    # PROCESAMIENTO DEL EVENTO
-    for evento in pygame.event.get(): 
-        if evento.type == pygame.QUIT: 
-            hecho = True
-         
-        # Acciones posibles del joystick: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if evento.type == pygame.JOYBUTTONDOWN:
-            print("Boton presionado del joystick.")
-        if evento.type == pygame.JOYBUTTONUP:
-            print("Boton liberado del joystick.")
-             
-  
-    # DIBUJAMOS
-    # Primero, limpiamos la pantalla con color blanco. No pongas otros comandos de dibujo 
-    # encima de esto, de lo contrario seran borrados por el comando siguiente.
-    pantalla.fill(BLANCO)
-    text_print.reset()
- 
-    # Contamos el numero de joysticks
-    joystick_count = pygame.joystick.get_count()
- 
-    text_print.print(pantalla, "Numero de joysticks: {}".format(joystick_count) )
-    text_print.indent()
-     
-    # Para cada joystick:
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-     
-        text_print.print(pantalla, "Joystick {}".format(i) )
-        text_print.indent()
-     
-        # Obtiene el nombre del Sistema Operativo del controlador/joystick
-        nombre = joystick.get_name()
-        text_print.print(pantalla, "Nombre del joystick: {}".format(nombre))
-         
-        # Habitualmente, los ejes van en pareja, arriba/abajo para uno, e izquierda/derecha
-        # para el otro.
-        ejes = joystick.get_numaxes()
-        text_print.print(pantalla, "Numero de ejes: {}".format(ejes))
-        text_print.indent()
-         
-        CMD = ''
-        Dir1 = 'f'
-        Dir2 = 'f'
-        POWER_M1 = 0
-        POWER_M2 = 0
-         
-        for i in range(ejes):
-            eje = joystick.get_axis(i)
-		    
-            
-            
-            if(i == 1):
-			    #valor del primer eje
-                POWER_M2 = int(abs(eje*253))
-                if(eje < 0):
-                    #valor menor que 0
-                    Dir2 = 'r'
-                else:
-                    #valor mayor que 0
-                    Dir2 = 'f'
-            
-            if(i == 3):
-                #valor del segundo eje
-                POWER_M1 = int(abs(eje*253))
-                if(eje < 0):
-                    #valor menor que 0
-                    Dir1 = 'r'
-                else:
-                    #valor mayor que 0
-                    Dir1 = 'f'
-            text_print.print(pantalla, "Eje {} valor: {:>6.3f}".format(i, eje))
-        text_print.unindent()
-         
-        if(Dir1 == 'f' and Dir2 == 'f'):
-            CMD = 'c' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
-            #print(CMD)
-            s.send(CMD.encode("utf-8"))
-        if(Dir1 == 'f' and Dir2 == 'r'):
-            CMD = 'e' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
-            #print(CMD)
-            s.send(CMD.encode("utf-8"))
-        if(Dir1 == 'r' and Dir2 == 'f'):
-            CMD = 'd' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
-            #print(CMD)
-            s.send(CMD.encode("utf-8"))
-        if(Dir1 == 'r' and Dir2 == 'r'):
-            CMD = 'b' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
-            #print(CMD)
-            s.send(CMD.encode("utf-8"))
-         
-        time.sleep(0.2)
-         
-        botones = joystick.get_numbuttons()
-        text_print.print(pantalla, "Numero de botones: {}".format(botones))
-        text_print.indent()
-         
-        for i in range(botones):
-            boton = joystick.get_button(i)
-            text_print.print(pantalla, "Boton {:>2} valor: {}".format(i,boton))
-        text_print.unindent()
-             
-        # Hat switch. Todo o nada para la direccion, no como en los joysticks.
-        # El valor vuelve en un array.
-        hats = joystick.get_numhats()
-        text_print.print(pantalla, "Numero de hats: {}".format(hats))
-        text_print.indent()
- 
-        for i in range(hats):
-            hat = joystick.get_hat(i)
-            text_print.print(pantalla, "Hat {} valor: {}".format(i, str(hat)))
-        text_print.unindent()
-         
-        text_print.unindent()
- 
-     
-    # TODO EL CODIGO DE DIBUJO DEBERIA IR ENCIMA DE ESTE COMENTARIO
-     
-    # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
-    pygame.display.flip()
- 
-    # Limitamos a 60 fotogramas por segundo.
-    reloj.tick(60)
-     
-pygame.quit()
+
+
+def handleConfigs():
+	configs = {
+	}
+	newParameters = parseLineCommands()
+	nconfigs = mapCommands(configs, newParameters)
+	return nconfigs
+
+def parseLineCommands():	
+	#loop over each arg to build the relevant args list
+	inputParameters = {
+	}
+	for argStr in sys.argv:
+		if argStr == "Jetbot_Joystick.py":
+			continue
+		argStr = argStr.replace(' ','')
+		tokens = argStr.split('=')
+		if len(tokens) != 2:
+			print("arg: '" + argStr + "' unknown format")
+			sys.exit(0)
+		inputParameters[tokens[0]] = tokens[1]
+	return inputParameters
+
+def mapCommands(currentConfigs, additionalArgs):
+	#Map current configs into the object and stores it
+	# is there a better approach?? ToDo: review
+	for key, value in additionalArgs.items():
+		if key == "--mode":
+			currentConfigs["joystickMode"] = value
+		elif key == "--host":
+			currentConfigs["host"] = int(value)
+		elif key == "--port":
+			currentConfigs["port"] = value
+		else:
+			print("WARNING: command '" + key + "' still not defined, ignored!")
+	return currentConfigs
+
+def initSocketClient(configs):
+	client = socketClientW()
+	client.connect(configs['host'], configs['port'])
+	return client
+
+def userValuesMap(socketObj, configs, userCmds):
+	mappedStr = ""
+	#ToDo:  handle multiple joysticks For now just takes the first found joystick in the array
+	mode = configs["joystickMode"]
+	cmd = userCmds[0]
+	if mode == "tank":
+		mappedStr = tankMode(cmd)
+	elif mode == "car":
+		mappedStr = carMode(cmd)
+	else:
+		print("ERROR: '" + mode + "' is not defined!")
+		sys.exit(1)
+	
+	socketObj.send(mappedStr.encode("utf-8"))
+	return True
+
+def tankMode(cmdObj):
+	CMD = ''
+	Dir1 = 'f'
+	Dir2 = 'f'
+	POWER_M1 = 0
+	POWER_M2 = 0
+	
+	for key, value in additionalArgs.items():
+		if key == "axis1":
+			POWER_M2 = int(abs(eje*253))
+			if(eje < 0):
+				#valor menor que 0
+				Dir2 = 'r'
+			else:
+				#valor mayor que 0
+				Dir2 = 'f'
+		elif key == "axis3":
+			#valor del segundo eje
+			POWER_M1 = int(abs(eje*253))
+			if(eje < 0):
+				#valor menor que 0
+				Dir1 = 'r'
+			else:
+				#valor mayor que 0
+				Dir1 = 'f'
+	
+	if(Dir1 == 'f' and Dir2 == 'f'):
+		CMD = 'c' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
+	if(Dir1 == 'f' and Dir2 == 'r'):
+		CMD = 'e' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
+	if(Dir1 == 'r' and Dir2 == 'f'):
+		CMD = 'd' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
+	if(Dir1 == 'r' and Dir2 == 'r'):
+		CMD = 'b' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
+	
+	#print(CMD)
+	#s.send(CMD.encode("utf-8"))
+	return CMD
+
+def carMode(cmdObj):
+	return str
+
+#ToDo: add the single joystick mapper for the single joystick mode
+
+if __name__ == "__main__":
+	configs = handleConfigs()
+	socketClient = initSocketClient(configs)
+	joystickUiObj = uiPygameJetbot()
+	joystickUiObj.pygameInit()
+	
+	while True:
+		userCmds, exitSignal = joystickUiObj.loop()
+		if(exitSignal):
+			break
+		userValuesMap(socketClient, configs, userCmds)
+	socketClient.close()
+	pass
