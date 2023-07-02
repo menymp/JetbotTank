c95e741b1bc87b6d5c1d17426a7dbe5795420c59
menymp <lmeny04@hotmail.com>
2023-07-02 10:48:33 -0600

    add initial linear transform for single joystick operation

--- ControlClient/Jetbot_Joystick.py
+++ ControlClient/Jetbot_Joystick.py
@@ -8,6 +8,7 @@ import numpy as np
 import urllib.request as request
 import cv2
 import sys
+import math
 sys.path.append("../JetbotServer")
 
 from socketServerWrapper import socketClientW
@@ -118,11 +119,36 @@ def tankMode(cmdObj):
 		CMD = 'b' + ',' + str(POWER_M1)+','+str(POWER_M2)+'#'
 	
 	#print(CMD)
+	#l,r = singleJoystickTransform()
+	#print("single mode: " + )
 	#s.send(CMD.encode("utf-8"))
 	return CMD
 
 def carMode(cmdObj):
 	return str
+
+
+def singleJoystickTransform(x, y):
+    # convert to polar
+    r = math.hypot(x, y)
+    t = math.atan2(y, x)
+
+    # rotate by 45 degrees
+    t += math.pi / 4
+
+    # back to cartesian
+    left = r * math.cos(t)
+    right = r * math.sin(t)
+
+    # rescale the new coords
+    left = left * math.sqrt(2)
+    right = right * math.sqrt(2)
+
+    # clamp to -1/+1
+    left = max(-1, min(left, 1))
+    right = max(-1, min(right, 1))
+
+    return left, right
 
 def url_to_image(url):
 	resp = request.urlopen(url)
