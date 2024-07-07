d727c50e0b4c09057a7d00905271adf3089349f4
menymp <lmeny04@hotmail.com>
2024-07-07 09:44:15 -0600

    notes for integration attempt

--- ROS/catkin_ws/src/remote_control/setup.py
+++ ROS/catkin_ws/src/remote_control/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
+#Attempting to know how to integrate the current sources to ROS
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['baseConfigs', 'baseStateMessage', 'BaseVideoService', 'CameraHttpServer', 'cameraServerController', ''],
+    packages=['baseConfigs', 'baseStateMessage', 'BaseVideoService', 'CameraHttpServer', 'cameraServerController', 'configsJetBotUtils', 'Jetbot_Joystick', 'joystickUtills', 'listener', 'remoteControlClient', 'serialControl', 'serialPortUtills', 'serialPortUtills', 'talker'],
     package_dir={'': 'scripts'}
 )
 setup(**d)
