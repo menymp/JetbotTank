c4a42d153538e6aad635ef1cb0d92e3fbed0fce1
menymp <lmeny04@hotmail.com>
2024-07-13 22:46:57 -0600

    add licence, change configs, remove non needed comments

--- ROS/catkin_ws/src/remote_control/setup.py
+++ ROS/catkin_ws/src/remote_control/setup.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
+from os.path import dirname, abspath, basename
 from distutils.core import setup
-#Attempting to know how to integrate the current sources to ROS
 from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['remote_control'],
-    package_dir={'': 'src'}
+
+setup_args = generate_distutils_setup(
+    packages=[basename(dirname(abspath(__file__)))],
+    package_dir={'': 'src'},
 )
-setup(**d)
+
+setup(**setup_args)
