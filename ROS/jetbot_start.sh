181af958563f7b5c418f3e1b8de04bbe7ddc46ae
menymp <lmeny04@hotmail.com>
2024-07-25 22:09:33 -0600

    add windows and linux initial startup scripts and files

---
+++ ROS/jetbot_start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# this script perform the jetbot control and mapping server startup for all services
+# menymp 25 Jul 2024
+#
+echo "ROS Jetbot server startup script"
+# source the ROS environment first
+source ./catkin_ws/devel/setup.bash
+# get local system ip
+ipaddr=$(ifconfig wlan0 | awk '/inet addr:/ {gsub("addr:","",$2); print $2}')
+#export environment variables
+export ROS_IP="$ipaddr"
+export ROS_MASTER_URI="http://$ipaddr:11311"
+
+# start the ros core process
+roscore &
+
+# start ROS bridge services for windows communication
+roslaunch rosbridge_server rosbridge_websocket.launch &
+rosrun tf2_web_republisher tf2_web_republisher &
+
+# start lidar service
+roslaunch hector_slam_launch jetson_nano.launch &
+
+# start jetbot remote control server
+roslaunch remote_control remote_server.launch &
---
+++ ROS/windows_utilities/windows_start.bat
@@ -0,0 +1,19 @@
+:: windows startup BAT for ROS jetbot tank, this should be executed under ROS.exe shell
+:: arguments
+::  %1 --- ros jetbot ip
+::
+:: menymp 25 Jul 2024
+
+ECHO ROS Jetbot Windows client startup script.
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+ECHO Master ip is %1  Local ip is %NetworkIP%
+:: set environment variables
+set ROS_IP=%NetworkIP%
+:: windows does not use double quotes
+set ROS_MASTER_URI=http://%1:11311
+
+:: start lidar service
+START /B roslaunch ./windows_rviz.launch
+
+:: start windows jetbot joystick remote control client
+START /B python ./Jetbot_Joystick.py --host=%1 --port=9090 --videoPort=8990 --mode=tank --joystickMode=tank
